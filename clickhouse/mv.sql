9f787dd740e8fb694b99bc652e889f4749710dc6
Shuai Shao <shuaishao93@gmail.com>
2024-07-23 15:36:56 -0700

    Update mv.sql

--- clickhouse/mv.sql
+++ clickhouse/mv.sql
@@ -16,3 +16,12 @@ FROM wikistat
 GROUP BY
     date,
     project;
+
+INSERT INTO wikistat_top_projects SELECT
+    date(time) AS date,
+    project,
+    sum(hits) AS hits
+FROM wikistat
+GROUP BY
+    date,
+    project
