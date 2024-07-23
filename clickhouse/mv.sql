4f038b1840e5fb8274ad60b6d3fdee81674f59b7
Shuai Shao <shuaishao93@gmail.com>
2024-07-23 15:28:00 -0700

    Create mv.sql

---
+++ clickhouse/mv.sql
@@ -0,0 +1,18 @@
+CREATE TABLE wikistat_top_projects
+(
+    `date` Date,
+    `project` LowCardinality(String),
+    `hits` UInt32
+)
+ENGINE = SummingMergeTree
+ORDER BY (date, project);
+
+CREATE MATERIALIZED VIEW wikistat_top_projects_mv TO wikistat_top_projects AS
+SELECT
+    date(time) AS date,
+    project,
+    sum(hits) AS hits
+FROM wikistat
+GROUP BY
+    date,
+    project;
