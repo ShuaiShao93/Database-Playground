156f8d1a52ce88e2ba7ac9ce5c993c475e3e8faf
Shuai Shao <shuaishao93@gmail.com>
2024-07-23 16:08:43 -0700

    comment

--- clickhouse/basic/insert.sql
+++ clickhouse/basic/insert.sql
@@ -6,5 +6,12 @@ VALUES(now(), 'test', '', '', 10),
 
 SELECT hits
 FROM wikistat_top_projects
-FINAL  --- FINAL merges the results with the same keys before returning them, otherwise you will see multiple rows with the same key
+/*
+FINAL merges the results with the same keys before returning them,
+otherwise you will see multiple rows with the same key.
+
+Note that this doesn't change the data in storage, so if you run
+the same query without final again, you still see multiple rows.
+*/
+FINAL
 WHERE (project = 'test') AND (date = date(now()))
